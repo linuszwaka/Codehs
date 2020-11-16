26baed73ed5f213a47f164b66db0eee9a87ceb13
linuszwaka <74074433+linuszwaka@users.noreply.github.com>
2020-11-16 14:10:52 -0500

    Create Circle.java

---
+++ 2.10.6/Circle.java
@@ -0,0 +1,29 @@
+public class Circle
+{
+    private double radius;
+    
+    public Circle(double theRadius)
+    {
+        radius = theRadius;
+    }
+
+    // Implement getArea using
+    // Math.PI and
+    // Math.pow
+    // Area = pi * r^2
+    public double getArea()
+    {
+        double area = Math.PI * (radius * radius);
+        return area;
+    }
+    
+    // Implement getCircumference using
+    // Math.PI
+    // Circumference = 2 * PI * r
+    public double getCircumference()
+    {
+        double circumference = Math.PI * 2*radius;
+        return circumference;
+    }
+    
+}
