4a652f65ac1e4ccf8a2c212d80281ae7e06220aa
linuszwaka <74074433+linuszwaka@users.noreply.github.com>
2020-11-16 14:09:58 -0500

    Create ExtremeMain.java

---
+++ 2.9.8/ExtremeMain.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.lang.*;
+
+public class ExtremeMain
+{
+    public static void main(String[] args)
+    {
+        // Create a Scanner object
+        Scanner input = new Scanner(System.in);
+        
+        // Create an Extremes object
+        Extremes hi = new Extremes();
+        
+        // Ask the user to guess the maximum value of an Integer
+        System.out.println("Guess the maximum Integer value: ");
+        int number = input.nextInt();
+        
+        // Compute and display the difference
+        // between the max and the guess
+        
+        System.out.println("You were off by " + hi.maxDiff(number));
+        
+        // Ask the user to guess the minimum value of an Integer
+        System.out.println("Guess the minimum Integer value: ");
+        int second = input.nextInt();
+        
+        // Compute and display the difference 
+        // between the min and the guess
+        
+        System.out.println("You were off by " + hi.minDiff(second));
+        
+    }
+}
